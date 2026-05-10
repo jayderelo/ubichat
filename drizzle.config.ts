bba4f2b2c63b871cda7495d5b8a4b84b514996fa
Jayvee de Relo <jayvee.derelo@awsys-i.com>
2026-05-10 16:08:38 +0800

    configure database

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./database/migrations",
+  schema: "./database/schema",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import * as schema from "./auth-schema";
+import * as schema from "../../database/schema/auth-schema";
 
 const db = drizzle(new Pool({ connectionString: process.env.DATABASE_URL }), {
   schema,
@@ -10,7 +10,7 @@ const db = drizzle(new Pool({ connectionString: process.env.DATABASE_URL }), {
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: "pg", schema }),
-  baseURL: "http://localhost:3000/",
+  baseURL: process.env.BETTER_AUTH_URL,
   socialProviders: {
     github: {
       clientId: process.env.GITHUB_CLIENT_ID!,
